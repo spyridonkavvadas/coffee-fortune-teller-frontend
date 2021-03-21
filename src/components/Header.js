9815a7f306f1eb658ab4463c7dda75003e803fdb
Spyridon Kavvadas <kavvadasspyridon@gmail.com>
2021-03-21 04:25:13 +0100

    Always fixing mobile layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,11 +16,13 @@ function Header() {
       <div class="col-2"></div>
 
       <div class="instructions col-8">
+        
         <ul>
             <li>Upload a photo</li>
-            <li>Enter Name, birthday, genter, and status</li>
+            <li>Enter Name, Birthday, Genter, and Status</li>
             <li>Press the button "Get Oracle"</li>
         </ul>
+        
       </div>
 
       <div class="col-2"></div>
